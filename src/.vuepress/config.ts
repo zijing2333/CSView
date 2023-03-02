87758b7cc648f62719d45a14e8e4c311049339ab
zijing2333 <qq944741457@gmail.com>
2023-03-02 15:24:04 +0800

    deploy

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -28,6 +28,8 @@ export default defineUserConfig({
       "script",
       {},
       `\
+      <!-- Matomo -->
+      <script>
         var _paq = window._paq = window._paq || [];
         /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
         _paq.push(['trackPageView']);
@@ -38,10 +40,11 @@ export default defineUserConfig({
           _paq.push(['setSiteId', '1']);
           var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
           g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-        })();   
+        })();
+      </script>
+      <!-- End Matomo Code -->
       `,
-    ],
-    
+    ],   
   ],
 
 
